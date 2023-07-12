caa6ee90622d763655c0e4cb2460afd5bbd65c3e
Webert Maximiano <webertcoach@gmail.com>
2023-07-12 18:18:01 -0300

    correcao codigo do professor desatualizado

--- projeto-modular/preprocessing/normalize.py
+++ projeto-modular/preprocessing/normalize.py
@@ -1,7 +1,14 @@
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 def normalize(data):
-    scaler = MinMaxScaler()
-    scaler.fit(data)
+    # Remove colunas não numéricas
+    numeric_data = data.select_dtypes(include=[np.number])
 
-    return scaler.transform(data)
+    scaler = MinMaxScaler()
+    scaler.fit(numeric_data)
+
+    normalized_data = scaler.transform(numeric_data)
+    data[numeric_data.columns] = normalized_data
+
+    return data
